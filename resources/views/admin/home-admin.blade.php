2632872de4163724bd96f3f497f193b701901de6
GraciaRosalinaHindirwan <Rosalinagracia331@gmail.com>
2026-03-05 21:21:53 +0700

    sebelum push github

--- resources/views/admin/home-admin.blade.php
+++ resources/views/admin/home-admin.blade.php
@@ -11,45 +11,12 @@
                     <p class="text-[var(--color-text)] font-semibold" style="font-size: var(--h2-size)">Dashboard Overview</p>
                     <p class="text-[var(--color-text)]" style="font-size: var(--caption)">Ringkasan data sistem platform properti</p>                </div>
                 </div>
+                
 
-             <div class="flex gap-[32px] mt-[40px]">
-                <div class="w-[260px]
-                p-[24px]
-                rounded-[16px]
-                relative
-                border
-                border-transparent
-                bg-[#0c0c0c]
-                shadow-[0_0_20px_rgba(168,85,247,0.15)]" style="background-image:
-                linear-gradient(#0c0c0c,#0c0c0c),
-                linear-gradient(135deg,#7c3aed,#ec4899);
-                background-origin:border-box;
-                background-clip:padding-box,border-box;
+            @include("components/admin/summary-card")
+             @include("components/admin/summary-card")
+             
 
-                box-shadow:0 0 20px rgba(168,85,247,0.15);">
-                    <div class="card-top">
-                        <div class="icon-box">
-                            <i class="bi bi-people"></i>
-                        </div>
-                        <span>Total Agen</span>
-                    </div>
-
-                    <div class="card-number">
-                        124
-                    </div>
-                </div>
-
-                <div class="card-stat">
-                    <div class="card-top">
-                        <div class="icon-box">
-                            <i class="bi bi-person-badge"></i>
-                        </div>
-                        <span>Total Notaris</span>
-                    </div>
-                    <div class="card-number">
-                        45
-                    </div>
-                </div>
             </div>
         </div>
 
---
+++ resources/views/components/admin/summary-card.blade.php
@@ -0,0 +1,27 @@
+<div class="flex gap-[32px] mt-[40px]">
+                <div class="w-[260px]
+                p-[24px]
+                rounded-[16px]
+                relative
+                border
+                border-transparent
+                bg-[#0c0c0c]
+                shadow-[0_0_20px_rgba(168,85,247,0.15)]" style="background-image:
+                linear-gradient(#0c0c0c,#0c0c0c),
+                linear-gradient(135deg,#7c3aed,#ec4899);
+                background-origin:border-box;
+                background-clip:padding-box,border-box;
+
+                box-shadow:0 0 20px rgba(168,85,247,0.15);">
+                    <div class="flex items-center gap-[12px] text-[var(--color-text)] font-medium">
+                        <div class="size-[40px] rounded-md flex items-center justify-center border border-[var(--btn-primary)] text-[var(--btn-gradient-logo)]" style="font-size: 20px;">
+                            <i class="bi bi-people"></i>
+                        </div>
+                        <span>Total Agen</span>
+                    </div>
+
+                    <div class="font-semibold mt-[20px] text-[var(--color-text)]" style="font-size:48px;">
+                        124
+                    </div>
+                </div>
+            </div>
