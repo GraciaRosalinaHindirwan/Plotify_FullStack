e1481682611ebf11b94675cd1285989cf3e8f6cb
digheag <digheagumaisa@gmail.com>
2026-04-16 20:18:00 +0700

    add method-transaction

--- resources/views/users/method-transaction.blade.php
+++ resources/views/users/method-transaction.blade.php
@@ -1,5 +1,84 @@
-@extends("layouts/property")
-@section("content")
+@extends("layouts/users")
 
-<h1>Test</h1>
+@section("content")
+<div class="p-[80px] bg-black">
+    
+    <div class="flex gap-10 justify-center">
+
+        <div class="w-[500px] bg-[#1E1E1E] border border-[#0E21A0] rounded-2xl p-6 flex flex-col gap-6">
+            <div>
+                <h2 class="text-2xl font-bold text-[#EFECE3]">Transaksi Langsung</h2>
+                <p class="text-sm text-gray-300 mt-2">
+                    Dengan memilih transaksi langsung, Anda akan langsung masuk ke tahap pembayaran dan verifikasi dokumen sesuai prosedur yang berlaku.
+                </p>
+            </div>
+
+            <div class="flex flex-col gap-2 text-[#EFECE3] text-sm">
+            <div class="flex items-center gap-2">
+                <img src="/img/star-icon.png" class="w-5 h-5">
+                <span>Harga transparan & final</span>
+            </div>
+
+            <div class="flex items-center gap-2">
+                <img src="/img/star-icon.png" class="w-5 h-5">
+                <span>Mempercepat legalitas</span>
+            </div>
+
+            <div class="flex items-center gap-2">
+                <img src="/img/star-icon.png" class="w-5 h-5">
+                <span>Proses lebih cepat</span>
+            </div>
+
+            <div class="flex items-center gap-2">
+                <img src="/img/star-icon.png" class="w-5 h-5">
+                <span>Cocok untuk pembeli serius</span>
+            </div>
+
+                </div>
+
+            <a href="http://" 
+            class="mt-2 block border border-blue-500 rounded-lg py-2 text-pink-400 text-center">
+                Ajukan Penawaran
+            </a>
+        </div>
+
+        <div class="w-[500px] bg-[#1E1E1E] border border-[#0E21A0] rounded-2xl p-6 flex flex-col gap-6">
+            <div>
+                <h2 class="text-2xl font-bold text-[#EFECE3]">Transaksi Negosiasi</h2>
+                <p class="text-sm text-gray-300 mt-2">
+                    Setelah penawaran dikirim, penjual dapat menerima, menolak, atau memberikan penawaran balik.
+                </p>
+            </div>
+
+            <div class="flex flex-col gap-2 text-[#EFECE3] text-sm">
+                <div class="flex flex-col gap-2 text-[#EFECE3] text-sm">
+            <div class="flex items-center gap-2">
+                <img src="/img/star-icon.png" class="w-5 h-5">
+                <span>Fleksibel & transparan</span>
+            </div>
+
+            <div class="flex items-center gap-2">
+                <img src="/img/star-icon.png" class="w-5 h-5">
+                <span>Bisa mendapatkan harga terbaik</span>
+            </div>
+
+            <div class="flex items-center gap-2">
+                <img src="/img/star-icon.png" class="w-5 h-5">
+                <span>Tersedia riwayat negosiasi</span>
+            </div>
+
+            <div class="flex items-center gap-2">
+                <img src="/img/star-icon.png" class="w-5 h-5">
+                <span>Cocok untuk diskusi harga</span>
+            </div>
+                </div>
+            <a href="http://" 
+            class="mt-10 block border border-blue-500 rounded-lg py-2 text-pink-400 text-center">
+                Ajukan Penawaran
+            </a>
+        </div>
+
+    </div>
+
+</div>
 @endsection
