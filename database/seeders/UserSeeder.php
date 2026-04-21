8c306ebcc363e01a494536f865fc4bd085d88ad0
GraciaRosalinaHindirwan <Rosalinagracia331@gmail.com>
2026-04-21 22:13:57 +0700

    bikin akun untuk masing masing role biar g capek

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,5 +22,41 @@ class UserSeeder extends Seeder
             'role' => 'admin',
             'email_verified_at' => now(),
             'password' => '12345678',]);
+
+        User::firstorCreate(
+            ['email' => 'catluminate@gmail.com'],
+            [
+                'fullname' => 'Catluminate',
+                'username' => 'catluminate',
+                'telp_number' => '081234567891',
+                'profile' => 'profile/1775103083.jpg',
+                'role' => 'users',
+                'email_verified_at' => now(),
+                'password' => '12345',
+            ]);
+        
+        User::firstorCreate(
+            ['email' => 'notary@gmail.com'],
+            [
+                'fullname' => 'Notary',
+                'username' => 'notary',
+                'telp_number' => '081234567892',
+                'profile' => 'profile/1775103083.jpg',
+                'role' => 'notary',
+                'email_verified_at' => now(),
+                'password' => '12345',
+            ]);
+        
+            User::firstorCreate(
+                ['email' => 'agent@gmail.com'],
+                [
+                    'fullname' => 'Agent',
+                    'username' => 'agent',
+                    'telp_number' => '081234567893',
+                    'profile' => 'profile/1775103083.jpg',
+                    'role' => 'agent',
+                    'email_verified_at' => now(),
+                    'password' => '12345',
+                ]);
     }
 }
