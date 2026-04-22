75e8ee6a5599cf0d6089ca638223426e8d34fc02
digheag <digheagumaisa@gmail.com>
2026-04-22 20:03:27 +0700

    adding success layout

--- resources/views/notary/add-AJB-successful.blade.php
+++ resources/views/notary/add-AJB-successful.blade.php
@@ -1,44 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AJB Success</title>
+@extends('layouts.success')
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@400;700&display=swap" rel="stylesheet">
+@section('message')
+File berhasil diunggah
+@endsection
 
-    @vite('resources/css/app.css')
-</head>
-
-<body class="bg-black">
-
-<div class="w-[1280px] p-[80px] flex flex-col justify-center items-start bg-black mx-auto">
-    <div class="flex flex-col items-start gap-[16px] self-stretch">
-        <img src="/img/Logo.png" class="w-[116px] h-[60px]" />
-    </div>
-
-    <div class="flex flex-col items-center justify-center self-stretch h-[778px] gap-[20px]">
-        <img src="/img/success.png"
-             class="w-[204px] h-[204px] flex-shrink-0 object-contain" />
-        <div class="flex flex-col justify-center items-center gap-[20px] self-stretch">
-            <div class="text-[#EFECE3] text-xl font-normal font-['Balsamiq_Sans'] text-center">
-                File berhasil diunggah
-            </div>
-        </div>
-              @include('components/admin/button', [
-                'href' => route('menu'),
-                'slot' => 'Kembali ke MENU'
-            ])  
-        
-            
-
-        </div>
-
-    </div>
-
-</div>
-
-</body>
-</html>
+@section('button')
+@include('components/admin/button', [
+    'href' => url('/menu'),
+    'slot' => 'Kembali ke MENU'
+])
+@endsection
