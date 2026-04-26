c5de4c0219d8ff45777a1d59bb1f2446084cd532
GraciaRosalinaHindirwan <Rosalinagracia331@gmail.com>
2026-04-26 14:06:54 +0700

    fixed navbar agent part 1

--- resources/views/components/common/navbar.blade.php
+++ resources/views/components/common/navbar.blade.php
@@ -1,10 +1,18 @@
+@php
+function isActive($route) {
+    return request()->is($route) 
+        ? 'bg-clip-text text-transparent bg-gradient-to-t from-[#B153D7] via-[#4D2FB2] to-[#0E21A0]' 
+        : 'class="text-[var(--color-text)] hover:opacity-70"';
+}
+@endphp
+
 <header class="sticky top-0 w-full pt-[80px] pb-[64px] bg-[var(--color-bg)]">
     <nav class="flex w-full justify-between items-center px-[80px]">
         <img src="/img/Logo.png" alt="Plotify logo">
 
         <ul class="flex items-center gap-[2rem]">
-            <li><a class="font-bold text-xl bg-clip-text text-transparent bg-gradient-to-t from-[#B153D7] via-[#4D2FB2] to-[#0E21A0]" href="">Janji Temu</a></li>
-            <li><a class="font-bold text-xl" href="">Permintaan</a></li>
+            <li><a class="font-bold text-xl {{ isActive('agent/appointment') }} transition-all duration-300" href="/agent/appointment">Janji Temu</a></li>
+            <li><a class="font-bold text-xl {{ isActive('agent/property') }} transition-all duration-300" href="/agent/property">Property</a></li>
             <li><a class="font-bold text-xl" href="">Publikasi</a></li>
             <li><a class="font-bold text-xl" href="">Dokumen</a></li>
             <li><a class="font-bold text-xl" href="">Penawaran</a></li>
